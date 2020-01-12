a33ab93da2e564978d660c16d74897f7d73078b5
Rafał Schmidt <rafalschmidt97@gmail.com>
2020-01-12 16:16:31 +0200

    feature: add global logger

--- src/main/java/fi/vamk/beceps/HelloController.java
+++ src/main/java/fi/vamk/beceps/HelloController.java
@@ -4,17 +4,18 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 
 @Controller()
-@Slf4j
 public class HelloController {
   @Get
   @Produces(MediaType.TEXT_PLAIN)
   public String index() {
-    val text = "Hello World request";
-    log.info(text);
     return "Hello World";
+  }
+
+  @Get("error")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String error() {
+    throw new RuntimeException("Try to catch me!");
   }
 }
---
+++ src/main/java/fi/vamk/beceps/core/trace/RequestTraceFilter.java
@@ -0,0 +1,34 @@
+package fi.vamk.beceps.core.trace;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MutableHttpResponse;
+import io.micronaut.http.annotation.Filter;
+import io.micronaut.http.filter.HttpServerFilter;
+import io.micronaut.http.filter.ServerFilterChain;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+
+@Filter("/**")
+@Slf4j
+public class RequestTraceFilter implements HttpServerFilter {
+  public Publisher<MutableHttpResponse<?>> doFilter(HttpRequest<?> request, ServerFilterChain chain) {
+    return Flowable.fromCallable(() -> {
+      if (!request.getPath().equals("/favicon.ico")) {
+        log.info(
+          "Request: {} {} {}",
+          request.getMethod(),
+          request.getUri(),
+          request.getBody().isPresent() ? request.getBody() : ""
+        );
+      }
+
+      return true;
+    }).subscribeOn(Schedulers.io())
+      .switchMap((aBoolean) -> chain.proceed(request))
+      .doOnNext(res ->
+        res.getHeaders().add("X-Trace-Enabled", "true")
+      );
+  }
+}
