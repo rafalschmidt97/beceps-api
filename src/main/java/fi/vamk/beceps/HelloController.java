2bec2ccb2e8464d11599ae6d398194971cace749
Rafał Schmidt <rafalschmidt97@gmail.com>
2020-01-12 15:38:20 +0200

    build: add lombok with example

--- src/main/java/fi/vamk/beceps/HelloController.java
+++ src/main/java/fi/vamk/beceps/HelloController.java
@@ -4,12 +4,17 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
 @Controller()
+@Slf4j
 public class HelloController {
   @Get
   @Produces(MediaType.TEXT_PLAIN)
   public String index() {
+    val text = "Hello World request";
+    log.info(text);
     return "Hello World";
   }
 }
