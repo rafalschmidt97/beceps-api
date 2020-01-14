6051a73329d1a735fdb0e709c179ead6e83c4dc9
Rafał Schmidt <rafalschmidt97@gmail.com>
2020-01-14 02:01:55 +0200

    feature: add swagger as openapi

--- src/main/java/fi/vamk/beceps/Application.java
+++ src/main/java/fi/vamk/beceps/Application.java
@@ -1,7 +1,22 @@
 package fi.vamk.beceps;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Beceps",
+        version = "1.0",
+        description = "Gym tool for scheduling workouts",
+        contact = @Contact(
+            name = "Rafał Schmidt",
+            url = "https://rafalschmidt.com",
+            email = "rafalschmidt97@gmail.com"
+        )
+    )
+)
 public class Application {
   public static void main(String[] args) {
     Micronaut.run(Application.class);
--- src/main/java/fi/vamk/beceps/accounts/AccountsController.java
+++ src/main/java/fi/vamk/beceps/accounts/AccountsController.java
@@ -4,12 +4,12 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import lombok.RequiredArgsConstructor;
 
-@Controller("accounts")
+@Controller("/accounts")
 @RequiredArgsConstructor
 public class AccountsController {
   private final AccountsService accountsService;
 
-  @Get("{id}")
+  @Get("/{id}")
   public AccountDto findById(Long id) {
     return accountsService.findById(id);
   }
