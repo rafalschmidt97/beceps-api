0aadc0c65e7a07557d8e386771840b73e5b142d0
Rafał Schmidt <rafalschmidt97@gmail.com>
2020-02-15 13:31:08 +0200

    bugfix: micronaut jdbc doesn't require ` for sets

--- src/main/java/fi/vamk/beceps/workouts/domain/Set.java
+++ src/main/java/fi/vamk/beceps/workouts/domain/Set.java
@@ -14,7 +14,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "`set`")
+@Table(name = "set")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
